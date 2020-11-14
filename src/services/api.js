cae1bf2d10ff017481f4fd1a509a63dcd157ce11
Marcelo Camilo <marcelohoc@gmail.com>
2020-11-14 14:41:02 -0300

    Adicionado proxy

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const password = process.env.PASSWD
 const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
 
 const api = axios.create( { 
-    baseURL: 'http://187.72.11.225:9095/rest',
+    baseURL: proxyurl + 'http://187.72.11.225:9095/rest',
     headers: {Authorization: `Basic ${token}`},
     params: {page: 1, pagesize: 20}
 });
